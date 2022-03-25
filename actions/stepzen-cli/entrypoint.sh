c4d4262bbda6109df24d746b68bf112323dd322c
Dan Debrunner <dan@stepzen.com>
2022-03-25 11:30:34 -0400

    Fix env name

--- actions/stepzen-cli/entrypoint.sh
+++ actions/stepzen-cli/entrypoint.sh
@@ -7,8 +7,7 @@ env
 
 set -e
 stepzen version
-set -x
-stepzen login --account "${SZ_ACCOUNT}" --adminkey "${SZ_ADMINKEY}"
+stepzen login --account "${SZ_ACCOUNT}" --adminkey "${SZ_ADMIN_KEY}"
 set -x
 stepzen upload --dir=$GITHUB_WORKSPACE/${sz_dir} schema ${sz_endpoint}
 stepzen deploy --schema ${sz_endpoint} ${sz_endpoint}
