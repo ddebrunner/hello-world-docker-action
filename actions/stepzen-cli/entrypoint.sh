98a7c909baa57c4f61379e6ac57adf68c9bbe252
Dan Debrunner <dan@stepzen.com>
2022-03-25 10:43:17 -0400

    Fix arg

--- actions/stepzen-cli/entrypoint.sh
+++ actions/stepzen-cli/entrypoint.sh
@@ -5,7 +5,7 @@ sz_endpoint=$2
 
 set -e
 stepzen version
-stepzen login --account "${SZ_ACCOUNT}" --admin "${SZ_ADMINKEY}"
+stepzen login --account "${SZ_ACCOUNT}" --adminkey "${SZ_ADMINKEY}"
 set -x
 stepzen upload --dir=$GITHUB_WORKSPACE/${sz_dir} schema ${sz_endpoint}
 stepzen deploy --schema ${sz_endpoint} ${sz_endpoint}
