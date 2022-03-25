c33c47f9c5195d86ebbfc42212a411ff9c5b3185
Dan Debrunner <dan@stepzen.com>
2022-03-25 11:25:11 -0400

    more set -x

--- actions/stepzen-cli/entrypoint.sh
+++ actions/stepzen-cli/entrypoint.sh
@@ -7,6 +7,7 @@ env
 
 set -e
 stepzen version
+set -x
 stepzen login --account "${SZ_ACCOUNT}" --adminkey "${SZ_ADMINKEY}"
 set -x
 stepzen upload --dir=$GITHUB_WORKSPACE/${sz_dir} schema ${sz_endpoint}
