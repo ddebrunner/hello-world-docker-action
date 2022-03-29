17228b3a4e0c55ae13c7b4fb7331b9534672a8ca
Dan Debrunner <dan@stepzen.com>
2022-03-29 11:39:50 -0400

    add postgres setup

--- actions/dvd/entrypoint.sh
+++ actions/dvd/entrypoint.sh
@@ -3,7 +3,8 @@
 set -x
 set -e
 
-find / -name '*entrypoint.sh'
+# Run the Postgres setup
+/usr/local/bin/docker-entrypoint.sh
 
 wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
 unzip dvdrental.zip
