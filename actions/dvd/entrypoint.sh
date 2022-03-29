a1f38749da4131b89c22235f5bb107d4a214ab07
Dan Debrunner <dan@stepzen.com>
2022-03-29 11:33:31 -0400

    dvdaction

---
+++ actions/dvd/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -l
+
+set -x
+set -e
+
+wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+unzip dvdrental.zip
+
+
+psql -U postgres -w --command="CREATE DATABASE dvdrental"
+pg_restore -U postgres -d dvdrental dvdrental.tar
+
+ls ${GITHUB_WORKSPACE}
