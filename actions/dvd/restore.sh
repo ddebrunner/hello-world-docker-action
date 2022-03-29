5f8f78aed36e52c5eaac3cc0ed2b9e7d34c51943
Dan Debrunner <dan@stepzen.com>
2022-03-29 15:14:38 -0400

    switch to script

---
+++ actions/dvd/restore.sh
@@ -0,0 +1,12 @@
+#!/bin/sh -l
+
+set -x
+set -e
+
+wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+unzip dvdrental.zip
+
+pg_restore -U postgres -d ${POSTGRES_DB} dvdrental.tar
+rm dvdrental.tar
+
+ls ${GITHUB_WORKSPACE}
