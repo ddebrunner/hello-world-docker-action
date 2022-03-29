d9e9a665424cb8d5e6af33679e80a38179430496
Dan Debrunner <dan@stepzen.com>
2022-03-29 15:47:54 -0400

    split files

--- actions/dvd/100_restore.sh
+++ actions/dvd/100_restore.sh
@@ -7,9 +7,7 @@ cd /tmp
 wget https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
 unzip dvdrental.zip
 
-pg_restore -U postgres -d ${POSTGRES_DB} dvdrental.tar
+pg_restore -U ${POSTGRES_USER} -d ${POSTGRES_DB} dvdrental.tar
 rm dvdrental.tar
 
 ls ${GITHUB_WORKSPACE}
-
-pg_ctl stop
---
+++ actions/dvd/999_stop.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -l
+pg_ctl stop
